cbac059c33f73cfcfd3ede49f24a16b4e184afbe
Jonathan Mauricenz <jonathan.mauricenz@gmail.com>
2023-10-04 19:36:17 -0300

    feat: configuration of unit tests with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000,
   },
-  plugins: [react()],
-});
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+  },
+})
